225a60ddd470ac58e73f4b97aa37591907b4239f
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2017-12-15 12:01:10 +0100

    changed decorators

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -9,7 +9,7 @@ from bluewhalecore.decorators import toplayer
 
 # ==========================================================
 # Test without decorators
-def make_dense_wo_decorator(inshapes, outshapes, params):
+def make_dense_wo_decorator(input, inshapes, outshapes, params):
     input = [Input(inshapes[name]['shape'], name=name)
              for name in inshapes]
     layer = Dense(params)(input[0])
@@ -22,7 +22,7 @@ def make_dense_wo_decorator(inshapes, outshapes, params):
 # ==========================================================
 # Test without output decorator
 @toplayer
-def make_dense_w_top(inshapes, outshapes, params):
+def make_dense_w_top(input, inshapes, outshapes, params):
     input = [Input(inshapes[name]['shape'], name=name)
              for name in inshapes]
     output = Dense(params)(input[0])
@@ -32,7 +32,7 @@ def make_dense_w_top(inshapes, outshapes, params):
 # ==========================================================
 # Test without input decorator
 @bottomlayer
-def make_dense_w_bottom(input, outshapes, params):
+def make_dense_w_bottom(input, inshapes, outshapes, params):
     input
     layer = Dense(params)(input[0])
     output = [Dense(outshapes[name]['shape'][0], name=name,
@@ -45,7 +45,7 @@ def make_dense_w_bottom(input, outshapes, params):
 # Test without input and output decorator
 @bottomlayer
 @toplayer
-def make_dense_w_topbottom(input, outshapes, params):
+def make_dense_w_topbottom(input, inputshapes, outshapes, params):
     output = Dense(params)(input[0])
     return input, output
 
@@ -57,11 +57,11 @@ def test_decorators():
 
     funclist = [make_dense_w_top, make_dense_w_bottom, make_dense_w_topbottom]
 
-    i, o = make_dense_wo_decorator(inp, oup, 30)
+    i, o = make_dense_wo_decorator(None, inp, oup, 30)
     ref_model = Model(i, o)
 
     for func in funclist:
-        i, o = func(inp, oup, 30)
+        i, o = func(None, inp, oup, 30)
         model = Model(i, o)
         for i in range(len(model.layers)):
             np.testing.assert_equal(model.layers[i].input_shape,
