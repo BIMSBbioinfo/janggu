41e25fb358a37ed36eb4770be03b9ce12bf38082
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2017-12-19 16:26:04 +0100

    added test for NumpyDataset

---
+++ tests/test_nparr.py
@@ -0,0 +1,11 @@
+import numpy as np
+from bluewhalecore.data.nparr import NumpyBwDataset
+
+def test_nparr(tmpdir):
+    X = NumpyBwDataset("X", np.random.random((1000, 100)))
+    y = NumpyBwDataset('y', np.random.randint(2, size=(1000, 1)))
+
+    np.testing.assert_equal(len(X), len(y))
+    np.testing.assert_equal(len(X), 1000)
+    np.testing.assert_equal(X.shape, (100,))
+    np.testing.assert_equal(y.shape, (1,))
