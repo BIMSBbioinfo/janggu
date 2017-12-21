2bac38925e55e08dfe7cec0eac114d0d0c79f7ab
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2017-12-21 15:22:11 +0100

    added BwGenomicIndexer

---
+++ src/bluewhalecore/data/genomic_indexer.py
@@ -0,0 +1,76 @@
+import os
+
+import numpy as np
+from HTSeq import GenomicInterval
+from genomeutils.regions import readBed
+from pandas import DataFrame
+
+
+class BwGenomicIndexer:
+    """ Maps genomic positions to the respective indices"""
+
+    def __init__(self, regions, resolution, stride):
+
+        self.resolution = resolution
+        self.stride = stride
+
+        # fill up int8 rep of DNA
+        # load dna, region index, and within region index
+        if isinstance(regions, str) and os.path.exists(regions):
+            regions_ = readBed(regions)
+        elif isinstance(regions, DataFrame):
+            regions_ = regions.copy()
+        else:
+            raise Exception('regions must be a bed-filename \
+                            or a pandas.DataFrame \
+                            containing the content of a bed-file')
+
+        # Create iregion index
+        reglens = ((regions_.end - regions_.start - resolution + stride) //
+                   stride).values
+
+        self.chrs = []
+        self.offsets = []
+        for i in range(len(reglens)):
+            self.chrs += [regions_.chr[i]] * reglens[i]
+            self.offsets += [regions_.start[i]] * reglens[i]
+
+        self.inregionidx = []
+        for i in range(len(reglens)):
+            self.inregionidx += range(reglens[i])
+
+    def __len__(self):
+        return len(self.chrs)
+
+    def __repr__(self):
+        return "BwGenomicIndexer(<regions>, " \
+            + "resolution={})".format(self.resolution)
+
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            start = self.offsets[index] + \
+                    self.inregionidx[index]*self.stride
+            return GenomicInterval(self.chrs[index], start,
+                                   start + self.resolution, '.')
+
+        raise NotImplementedError('GenomicIndexer indexing with "int" only')
+
+    def idxByChrom(self, include=[], exclude=[]):
+
+        if isinstance(include, str):
+            include = [include]
+
+        if isinstance(exclude, str):
+            exclude = [exclude]
+
+        if include == []:
+            idxs = set(range(len(self)))
+        else:
+            idxs = set()
+            for inc in include:
+                idxs.update(np.where(np.asarray(self.chrs) == inc)[0])
+
+        for exc in exclude:
+            idxs = idxs.difference(np.where(np.asarray(self.chrs) == exc)[0])
+
+        return list(idxs)
---
+++ tests/test_gindexer.py
@@ -0,0 +1,58 @@
+import os
+import pkg_resources
+
+import numpy as np
+from genomeutils.regions import readBed
+from bluewhalecore.data import BwGenomicIndexer
+
+
+def test_gindexer_merged():
+    data_path = pkg_resources.resource_filename('bluewhalecore', 'resources/')
+    regions = readBed(os.path.join(data_path, 'regions.bed'))
+
+    gi = BwGenomicIndexer(os.path.join(data_path, 'regions.bed'),
+                          resolution=200, stride=50)
+    np.testing.assert_equal(len(gi), 14344)
+
+    gi = BwGenomicIndexer(regions, resolution=200, stride=50)
+    np.testing.assert_equal(len(gi), 14344)
+
+    np.testing.assert_equal(len(gi.idxByChrom(include='chr1')), 14344)
+
+    np.testing.assert_equal(len(gi.idxByChrom(include='chr10')), 0)
+    np.testing.assert_equal(len(gi.idxByChrom(exclude='chr10')), 14344)
+    np.testing.assert_equal(len(gi.idxByChrom(exclude='chr1')), 0)
+
+    iv = gi[0]
+    np.testing.assert_equal((iv.chrom, iv.start, iv.end, iv.strand),
+                            ('chr1', 600, 800, '.'))
+
+    iv = gi[7]
+    np.testing.assert_equal((iv.chrom, iv.start, iv.end, iv.strand),
+                            ('chr1', 950, 1150, '.'))
+
+
+def test_gindexer_indiv():
+    data_path = pkg_resources.resource_filename('bluewhalecore', 'resources/')
+    regions = readBed(os.path.join(data_path, 'indiv_regions.bed'))
+
+    gi = BwGenomicIndexer(os.path.join(data_path, 'indiv_regions.bed'),
+                          resolution=200, stride=50)
+    np.testing.assert_equal(len(gi), 14344)
+
+    gi = BwGenomicIndexer(regions, resolution=200, stride=50)
+    np.testing.assert_equal(len(gi), 14344)
+
+    np.testing.assert_equal(len(gi.idxByChrom(include='chr1')), 14344)
+
+    np.testing.assert_equal(len(gi.idxByChrom(include='chr10')), 0)
+    np.testing.assert_equal(len(gi.idxByChrom(exclude='chr10')), 14344)
+    np.testing.assert_equal(len(gi.idxByChrom(exclude='chr1')), 0)
+
+    iv = gi[0]
+    np.testing.assert_equal((iv.chrom, iv.start, iv.end, iv.strand),
+                            ('chr1', 600, 800, '.'))
+
+    iv = gi[7]
+    np.testing.assert_equal((iv.chrom, iv.start, iv.end, iv.strand),
+                            ('chr1', 950, 1150, '.'))
