6ba5aa296e382505b1b52021de1834936cc03d5a
Wolfgang Kopp <kopp@molgen.mpg.de>
2018-01-03 17:30:52 +0100

    Using GenomicArray with DNA

--- src/bluewhalecore/data/dna.py
+++ src/bluewhalecore/data/dna.py
@@ -2,113 +2,139 @@ import os
 
 import numpy as np
 from data import BwDataset
-from genomeutils.regions import readBed
+from bluewhalecore.data import BwGenomicArray
+from bluewhalecore.data import BwGenomicIndexer
+from HTSeq import GenomicInterval
 from genomeutils.sequences import dna2ind
-from genomeutils.sequences import sequencesForRegions
 from genomeutils.sequences import sequencesFromFasta
 from pandas import DataFrame
 
 
 class DnaBwDataset(BwDataset):
 
-    def __init__(self, name, seqs, stride=50, reglen=200,
+    def __init__(self, name, seqs, gindexer,
                  flank=150, order=1, cachedir=None):
 
-        # self.seqs = seqs
-
-        self.stride = stride
-        self.reglen = reglen
         self.flank = flank
         self.order = order
-
-        # Create iregion index
-        reglens = np.asarray([(len(seq) - reglen -
-                               2*flank + stride) // stride for seq in seqs])
-
-        iregions = []
-        for i in range(len(reglens)):
-            iregions += [i] * reglens[i]
-
-        # create inregionidx
-        inregionidx = []
-        for i in range(len(reglens)):
-            inregionidx += range(reglens[i])
-
-        self.iregion = iregions
-        self.inregionidx = inregionidx
-
-        # Convert sequences to index array
-        print('Convert sequences to index array')
-        idna = []
-        for seq in seqs:
-            dna = np.asarray(dna2ind(seq), dtype='int8')
-            if self.order > 1:
-                # for higher order motifs, this part is used
-                filter = np.asarray([pow(4, i) for i in range(self.order)])
-                dna = np.convolve(dna, filter, mode='valid')
-            idna.append(dna)
-
-        self.idna = idna
-
-        self.cachedir = cachedir
+        self.seqs = seqs
+        self.gindexer = gindexer
 
         BwDataset.__init__(self, '{}'.format(name))
 
-    @classmethod
-    def extractRegionsFromRefGenome(cls, name, refgenome, regions,
-                                    stride=50, reglen=200,
-                                    flank=150, order=1, cachedir=None):
-        # fill up int8 rep of DNA
-        # load dna, region index, and within region index
-        if isinstance(regions, str) and os.path.exists(regions):
-            regions_ = readBed(regions)
-        elif isinstance(regions, DataFrame):
-            regions_ = regions.copy()
-        else:
-            raise Exception('regions must be a bed-filename \
-                            or a pandas.DataFrame \
-                            containing the content of a bed-file')
-
-        regions_.start -= flank
-        regions_.end += flank
-
+    @staticmethod
+    def _makeGenomicArray(name, fastafile, order, storage, cachedir=None,
+                          overwrite=False):
         # Load sequences from refgenome
-
-        print('Load sequences from ref genome')
-        seqs = sequencesForRegions(regions_, refgenome)
-
-        return cls(name, seqs, stride, reglen, flank, order)
-
-    @classmethod
-    def fromFasta(cls, name, fastafile, order=1, cachedir=None):
         seqs = sequencesFromFasta(fastafile)
 
-        reglen = len(seqs[0])
-        flank = 0
-        stride = 1
+        chromlens = {}
 
+        for seq in seqs:
+            chromlens[seq.chr] = len(seq) - order + 1
+
+        if storage != 'memmap' or storage != 'nparray':
+            raise Exception('storage must be memmap or nparray')
+
+        if storage == 'memmap':
+            memmap_dir = os.path.join(cachedir, name,
+                                      os.path.basename(fastafile))
+            if not os.path.exists(memmap_dir):
+                os.makedirs(memmap_dir)
+
+            ps = [os.path.join(memmap_dir, '{}..nmm'.format(x))
+                  for x in chromlens.keys()]
+            nmms = [os.path.exists(p) for p in ps]
+        else:
+            nmms = [False]
+            memmap_dir = ''
+
+        garray = BwGenomicArray(chromlens, stranded=False,
+                                typecode='int8',
+                                storage=storage, memmap_dir=memmap_dir,
+                                overwrite=overwrite)
+
+        if all(nmms) and not overwrite:
+            print('Reload BwGenomicArray from {}'.format(memmap_dir))
+        else:
+            # Convert sequences to index array
+            print('Convert sequences to index array')
+            for seq in seqs:
+                iv = GenomicInterval(seq.chr, 0, len(seq) - order + 1, '.')
+
+                dna = np.asarray(dna2ind(seq), dtype='int8')
+
+                if order > 1:
+                    # for higher order motifs, this part is used
+                    filter = np.asarray([pow(4, i) for i in range(order)])
+                    dna = np.convolve(dna, filter, mode='valid')
+
+                garray[iv] = dna
+
+        return garray
+
+    @classmethod
+    def fromRefGenome(cls, name, refgenome, regions,
+                      stride=50, reglen=200,
+                      flank=150, order=1, storage='memmap',
+                      cachedir=None, overwrite=False):
+        # fill up int8 rep of DNA
+        # load dna, region index, and within region index
+
+        gindexer = BwGenomicIndexer(regions, reglen, stride)
+
+        garray = cls._makeGenomicArray(name, refgenome, storage,
+                                       cachedir=cachedir,
+                                       overwrite=overwrite)
+
+        return cls(name, garray, gindexer, flank, order)
+
+    @classmethod
+    def fromFasta(cls, name, fastafile, storage='nparray',
+                  order=1, cachedir=None, overwrite=False):
+
+        garray = cls._makeGenomicArray(name, fastafile, storage,
+                                       cachedir=cachedir,
+                                       overwrite=overwrite)
+
+        seqs = sequencesFromFasta(fastafile)
+
+        # Check if sequences are equally long
         lens = [len(seq) for seq in seqs]
         assert lens == [len(seqs[0])] * len(seqs), "Input sequences must " + \
             "be of equal length."
 
-        return cls(name, seqs, stride, reglen, flank, order)
+        # Chromnames are required to be Unique
+        chroms = [seq.id for seq in seqs]
+        assert len(set(chroms)) == len(seqs), "Sequence IDs must be unique."
+        # now mimic a dataframe representing a bed file
+
+        regions = DataFrame({'chr': chroms, 'start': 0, 'end': lens})
+
+        reglen = lens[0]
+        flank = 0
+        stride = 1
+
+        gindexer = BwGenomicIndexer(regions, reglen, stride)
+
+        return cls(name, garray, gindexer, flank, order)
 
     def __repr__(self):
-        return 'DnaBwDataset("{}", <seqs>, <iregion>, <inregionidx>, \
-                stride={}, reglen={}, flank={})'\
-                .format(self.name, self.stride, self.reglen, self.flank)
+        return 'DnaBwDataset("{}", <seqs>, <gindexer>, \
+                flank={})'\
+                .format(self.name, self.flank)
 
     def idna4idx(self, idxs):
         # for each index read use the adaptor indices to retrieve the seq.
-        idna = np.empty((len(idxs), self.reglen +
+        idna = np.empty((len(idxs), self.gindexer.resolution +
                          2*self.flank - self.order + 1), dtype="int8")
 
-        # obtain idna
         for i, idx in enumerate(idxs):
-            idna[i] = self.idna[self.iregion[idx]][
-                        (self.inregionidx[idx]*self.stride):
-                        (self.inregionidx[idx]*self.stride
-                         + self.reglen + 2*self.flank - self.order + 1)]
+            iv = self.gindexer[idx]
+            iv.start -= self.flank
+            iv.end += self.flank - self.order + 1
+
+            idna[i] = np.asarray(list(self.garray[iv]))
 
         return idna
 
@@ -123,6 +149,11 @@ class DnaBwDataset(BwDataset):
         return onehot
 
     def __getitem__(self, idxs):
+        if isinstance(idxs, int):
+            idxs = [idxs]
+        if not isinstance(idxs, list):
+            raise IndexError('DnaBwDataset.__getitem__ '
+                             + 'requires "int" or "list"')
 
         data = self.as_onehot(self.idna4idx(idxs))
 
@@ -153,36 +184,6 @@ class DnaBwDataset(BwDataset):
         return locals()
 
     order = property(**order())
-
-    def stride():
-        doc = "The stride property."
-
-        def fget(self):
-            return self._stride
-
-        def fset(self, value):
-            if not isinstance(value, int) or value <= 0:
-                raise Exception('stride must be a positive integer')
-            self._stride = value
-
-        return locals()
-
-    stride = property(**stride())
-
-    def reglen():
-        doc = "The reglen property."
-
-        def fget(self):
-            return self._reglen
-
-        def fset(self, value):
-            if not isinstance(value, int) or value <= 0:
-                raise Exception('reglen must be a positive integer')
-            self._reglen = value
-
-        return locals()
-
-    reglen = property(**reglen())
 
     def flank():
         doc = "The flank property."
