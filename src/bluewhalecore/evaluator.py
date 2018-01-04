9d8c2eecc7966689dbb6611c01fc57df5baa589f
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2018-01-04 14:26:06 +0100

    fixed errors in evaluator

--- src/bluewhalecore/evaluator.py
+++ src/bluewhalecore/evaluator.py
@@ -2,6 +2,9 @@ import datetime
 
 from pymongo import MongoClient
 
+from .generators import bluewhale_fit_generator
+from .generators import bluewhale_predict_generator
+
 
 class Evaluator(object):
     """This class collects the results for a trained models records
@@ -14,29 +17,37 @@ class Evaluator(object):
 
     def _record(self, modelname, modeltags, metricname, value, datatags):
         item = {'date': datetime.datetime.utcnow(),
-                'modelname': self.bluewhale.name,
+                'modelname': modelname,
                 'measureName': metricname,
                 'measureValue': value,
                 'datatags': datatags,
                 'modeltags': modeltags}
 
-        iid = self.db.results.insert_one(item).insert_id
-        self.bluewhale.logger.info("Recorded {}".format(iid))
+        return self.db.results.insert_one(item).inserted_id
 
     def dump(self, bluewhale, xfeat, ytrue,
              elementwise_score={},
              combined_score={},
              datatags=[],
-             modeltags=[]):
+             modeltags=[],
+             batch_size=None,
+             use_multiprocessing=False):
 
         # record evaluate() results
         # This is done by default
-        val = bluewhale.evaluate(xfeat, ytrue)
+        val = bluewhale.evaluate(xfeat, ytrue, batch_size=batch_size,
+                                 generator=bluewhale_fit_generator,
+                                 workers=1,
+                                 use_multiprocessing=use_multiprocessing)
         for i, v in enumerate(val):
-            self._record(self.bluewhale.name, modeltags,
-                         self.bluewhale.metric_names[i], v, datatags)
+            iid = self._record(bluewhale.name, modeltags,
+                               bluewhale.metrics_names[i], v, datatags)
+            bluewhale.logger.info("Recorded {}".format(iid))
 
-        ypred = bluewhale.predict(xfeat)
+        ypred = bluewhale.predict(xfeat, batch_size=batch_size,
+                                  generator=bluewhale_predict_generator,
+                                  workers=1,
+                                  use_multiprocessing=use_multiprocessing)
 
         # record individual dimensions
         for key in elementwise_score:
@@ -44,9 +55,11 @@ class Evaluator(object):
                 val = elementwise_score[key](ytrue[:, s], ypred[:, s])
                 d = datatags
                 d.append(ytrue.samplenames)
-                self._record(self.bluewhale.name, modeltags, key, val, d)
+                iid = self._record(bluewhale.name, modeltags, key, val, d)
+                bluewhale.logger.info("Recorded {}".format(iid))
 
         # record additional combined scores
         for key in combined_score:
-            val = combined_score[key](ytrue, ypred)
-            self._record(self.bluewhale.name, modeltags, key, val, datatags)
+            val = combined_score[key](ytrue[:], ypred)
+            iid = self._record(bluewhale.name, modeltags, key, val, datatags)
+            bluewhale.logger.info("Recorded {}".format(iid))
