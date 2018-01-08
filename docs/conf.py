0d3627df9799c5af9c58f0b4b148971a80ad8758
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2018-01-08 14:37:22 +0100

    Bump version: 0.4.0 → 0.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = u'BlueWhaleCore'
 year = u'2017'
 author = u'Wolfgang Kopp'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.4.0'
+version = release = '0.4.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- src/bluewhalecore/__init__.py
+++ src/bluewhalecore/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 from .bluewhale import BlueWhale  # noqa
 from .decorators import inputlayer  # noqa
