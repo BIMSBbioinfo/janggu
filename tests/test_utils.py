ca576caa8f959e0b9b2f94ab20f653e5bc6e273f
Wolfgang Kopp <kopp@molgen.mpg.de>
2018-04-15 21:23:58 +0200

    test for get_genome_size

---
+++ tests/test_utils.py
@@ -0,0 +1,10 @@
+from os.path import join
+import pkg_resources
+import pytest
+from janggo.utils import get_genome_size
+
+def test_genome_size():
+    data_path = pkg_resources.resource_filename('janggo', 'resources/')
+    gsize = get_genome_size('sacCer3', data_path)
+    print(gsize)
+    assert gsize['chrXV'] == 1091291
