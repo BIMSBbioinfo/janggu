2962d91b00081b2f4177ea707b605fb230b66e0c
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2018-05-07 18:16:26 +0200

    Testing dna with gtf

--- tests/test_dna_dataset.py
+++ tests/test_dna_dataset.py
@@ -22,58 +22,12 @@ flank = 150
 stepsize = 50
 
 
-def _seqToOneHot(seqs):
-    onehots = []
-    for seq in seqs:
-        onehots.append(_getOneHotSeq(seq.seq))
-    return np.concatenate(onehots, axis=0)
-
-
-def _getOneHotSeq(seq):
-    m = len(seq.alphabet.letters)
-    n = len(seq)
-    result = np.zeros((1, n, m, 1), dtype="float32")
-    for i in range(len(seq)):
-        if NMAP[seq[i]] >= 0:
-            result[0, i, NMAP[seq[i]], 0] = 1
-    return result
-
-
 def datalen(bed_file):
     reglens = 0
     reader = BED_Reader(bed_file)
     for reg in reader:
         reglens += (reg.iv.end - reg.iv.start - reglen + stepsize)//stepsize
     return reglens
-
-
-def dna_templ(order):
-    data_path = pkg_resources.resource_filename('janggo', 'resources/')
-
-    bed_merged = os.path.join(data_path, 'sample.bed')
-
-    refgenome = os.path.join(data_path, 'sample_genome.fa')
-
-    data = Dna.create_from_refgenome('train', refgenome=refgenome,
-                                     regions=bed_merged,
-                                     storage='ndarray',
-                                     order=order)
-
-    # for order 1
-    assert len(data) == 100
-    assert data.shape == (100, 200, 4, 1)
-    np.testing.assert_equal(data[0][0, :10, :, 0],
-                            np.asarray([[1, 0, 0, 0],
-                                        [0, 0, 0, 1],
-                                        [0, 0, 0, 1],
-                                        [0, 0, 1, 0],
-                                        [0, 0, 0, 1],
-                                        [0, 0, 1, 0],
-                                        [0, 0, 1, 0],
-                                        [0, 0, 0, 1],
-                                        [0, 0, 1, 0],
-                                        [1, 0, 0, 0]], dtype=int8))
-
 
 def test_read_ranges_from_file():
 
@@ -110,7 +64,7 @@ def test_read_ranges_from_file():
 def test_dna_dims_order_1():
     order = 1
     data_path = pkg_resources.resource_filename('janggo', 'resources/')
-    bed_merged = os.path.join(data_path, 'sample.bed')
+    bed_merged = os.path.join(data_path, 'sample.gtf')
     refgenome = os.path.join(data_path, 'sample_genome.fa')
 
     data = Dna.create_from_refgenome('train', refgenome=refgenome,
@@ -474,15 +428,6 @@ def test_read_dna_from_fasta_order_2(tmpdir):
     np.testing.assert_equal(data[3][0, :5, :, :].sum(), 3)
 
 
-def test_stemcell_onehot_identity():
-    data_path = pkg_resources.resource_filename('janggo', 'resources/')
-
-    filename = os.path.join(data_path, 'oct4.fa')
-    data = Dna.create_from_fasta('dna', fastafile=filename)
-
-    oh = _seqToOneHot(sequences_from_fasta(filename))
-
-    np.testing.assert_equal(data[:], oh)
 
 
 def _dna_with_region_strandedness(order):
