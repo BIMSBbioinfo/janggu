649bc01f58443d79812e8623c6947f2e618b93b5
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2018-05-12 22:55:01 +0200

    added dash main page

--- src/janggo/cli.py
+++ src/janggo/cli.py
@@ -14,15 +14,43 @@ Why does this file exist, and why not put this in __main__?
 
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 import argparse
 
 PARSER = argparse.ArgumentParser(description='Command description.')
-PARSER.add_argument('names', metavar='NAME', nargs=argparse.ZERO_OR_MORE,
-                    help="A name of something.")
+PARSER.add_argument('-path', dest='janggo_results', 
+                    default=os.path.join(os.path.expanduser("~"), 
+                                         'janggo_results'),
+                    help="Janggo results path.")
+
+args = PARSER.parse_args(args=args)
+
+app = dash.Dash()
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+@app.callback(dash.dependencies.Output('page-content', 'children'),
+              [dash.dependencies.Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/':
+        files = glob.glob(args.janggo_results)
+        return html.Div([
+            html.H1('Janggo', href='/')
+            html.Br(),
+            html.Table(
+            [html.Tr([html.Th('Model')]),
+             [html.Tr(dcc.Link(name, href='/{}'.format(name)) for name in files] 
+            ])])
+    else:
+        return html.Div([html.H3('You are on page {}'.format(pathname))])
 
 
 def main(args=None):
-    args = PARSER.parse_args(args=args)
     print('Welcome to janggo (GPL-v2). Copyright (C) 2017 '
           + 'Wolfgang Kopp.')
-    print(args.names)
+    app.run_server()
