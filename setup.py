ee63e3a00cbc32e9222e8cb9473eea625ac4303a
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2019-11-13 12:32:01 +0100

    retired python 2.7 support

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
         'Operating System :: Unix',
         'Operating System :: POSIX',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
